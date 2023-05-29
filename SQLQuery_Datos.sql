4eff71482ec5ede68b96baace44dd0984789b40c
Edward Rodriguez <edwarderf@gmail.com>
2023-05-28 22:05:06 -0300

    Se quitan tildes en datos - Se modifica formato de fecha en inserts - Se modifican correos

--- SQLQuery_Datos.sql
+++ SQLQuery_Datos.sql
@@ -1,19 +1,21 @@
 USE DBCARGAS;
+SET DATEFORMAT DMY
+
 INSERT INTO Cliente (cliNom, cliMail, cliCantCargas)
 VALUES 
-  ('Leonardo da Vinci', 'leonardodavinci@mail', 10),
-  ('Pablo Picasso', 'pablopicasso@mail', 5),
-  ('Vincent van Gogh', 'vincentvangogh@mail', 8),
-  ('Frida Kahlo', 'fridakahlo@mail', 12),
-  ('Salvador Dalí', 'salvadordali@mail', 6),
-  ('Michelangelo Buonarroti', 'michelangelobuonarroti@mail', 9),
-  ('Claude Monet', 'claudemonet@mail', 7),
-  ('Rembrandt van Rijn', 'rembrandtvanrijn@mail', 11),
-  ('Edvard Munch', 'edvardmunch@mail', 3),
-  ('Claude Monet', 'claudemonet2@mail', 4);
+  ('Leonardo da Vinci', 'leonardodavinci@mail.com', 10),
+  ('Pablo Picasso', 'pablopicasso@mail.com', 5),
+  ('Vincent van Gogh', 'vincentvangogh@mail.com', 8),
+  ('Frida Kahlo', 'fridakahlo@mail.com', 12),
+  ('Salvador Dali', 'salvadordali@mail.com', 6),
+  ('Michelangelo Buonarroti', 'michelangelobuonarroti@mail.com', 9),
+  ('Claude Monet', 'claudemonet@mail.com', 7),
+  ('Rembrandt van Rijn', 'rembrandtvanrijn@mail.com', 11),
+  ('Edvard Munch', 'edvardmunch@mail.com', 3),
+  ('Claude Monet', 'claudemonet2@mail.com', 4);
 
 
-  INSERT INTO Avion (avionID, avionMAT, avionMarca, avionModelo, avionCapacidad)
+INSERT INTO Avion (avionID, avionMAT, avionMarca, avionModelo, avionCapacidad)
 VALUES 
   ('AVN001', 'MAT001', 'Boeing', '747', 150),
   ('AVN002', 'MAT002', 'Airbus', 'A380', 120),
@@ -26,7 +28,7 @@ VALUES
   ('AVN009', 'MAT009', 'Cessna', 'Citation Sovereign', 15),
   ('AVN010', 'MAT010', 'Bombardier', 'Global Express', 90);
 
-  INSERT INTO Dcontainer (dContID, dContLargo, dContAncho, dcontAlto, dcontCapacidad)
+INSERT INTO Dcontainer (dContID, dContLargo, dContAncho, dcontAlto, dcontCapacidad)
 VALUES 
   ('DC001', 2.0, 2.5, 1.8, 5),
   ('DC002', 2.5, 3.0, 2.0, 6),
@@ -41,47 +43,47 @@ VALUES
 
 INSERT INTO Aeropuerto (codIATA, aeroNombre, aeroPais)
 VALUES 
-  ('AER001', 'Aeropuerto de París-Charles de Gaulle', 'Francia'),
-  ('AER002', 'Aeropuerto de Londres-Heathrow', 'Reino Unido'),
-  ('AER003', 'Aeropuerto de Ámsterdam-Schiphol', 'Países Bajos'),
-  ('AER004', 'Aeropuerto de Fráncfort del Meno', 'Alemania'),
-  ('AER005', 'Aeropuerto de Madrid-Barajas Adolfo Suárez', 'España'),
-  ('AER006', 'Aeropuerto de Roma-Fiumicino', 'Italia'),
-  ('AER007', 'Aeropuerto de Estambul-Atatürk', 'Turquía'),
+  ('AER001', 'Aeropuerto de Paris-Charles de Gaulle', 'Francia'),
+  ('AER002', 'Aeropuerto de Londres Heathrow', 'Reino Unido'),
+  ('AER003', 'Aeropuerto de Amsterdam Schiphol', 'Paises Bajos'),
+  ('AER004', 'Aeropuerto de Francfort del Meno', 'Alemania'),
+  ('AER005', 'Aeropuerto de Madrid Barajas Adolfo Suarez', 'Espania'),
+  ('AER006', 'Aeropuerto de Roma Fiumicino', 'Italia'),
+  ('AER007', 'Aeropuerto de Estambul Ataturk', 'Turquia'),
   ('AER008', 'Aeropuerto de Atenas-Eleftherios Venizelos', 'Grecia'),
   ('AER009', 'Aeropuerto de Lisboa-Portela', 'Portugal'),
-  ('AER010', 'Aeropuerto de Dublín', 'Irlanda');
+  ('AER010', 'Aeropuerto de Dublin', 'Irlanda');
 
-
-  INSERT INTO Carga (avionID, dContID, cargaFch, cargaKilos, cliID, aeroOrigen, aeroDestino, cargaStatus)
-VALUES 
-  ('AVN001', 'DC001', '2023-05-10', 10000, 1, 'AER001', 'AER002', 'R'),
-  ('AVN001', 'DC002', '2023-05-12', 8000, 1, 'AER001', 'AER003', 'R'),
-  ('AVN002', 'DC003', '2023-05-11', 5000, 2, 'AER002', 'AER003', 'C'),
-  ('AVN003', 'DC004', '2023-05-13', 7000, 3, 'AER003', 'AER004', 'R'),
-  ('AVN004', 'DC005', '2023-05-14', 4000, 4, 'AER004', 'AER005', 'C'),
-  ('AVN005', 'DC006', '2023-05-15', 3000, 5, 'AER005', 'AER006', 'R'),
-  ('AVN006', 'DC007', '2023-05-16', 6000, 6, 'AER006', 'AER007', 'C'),
-  ('AVN007', 'DC008', '2023-05-17', 9000, 7, 'AER007', 'AER008', 'R'),
-  ('AVN008', 'DC009', '2023-05-18', 2000, 8, 'AER008', 'AER009', 'C'),
-  ('AVN009', 'DC010', '2023-05-19', 5000, 9, 'AER009', 'AER010', 'R');
-
-
-  /*ESTOS DÁRÍAN ERROR*/
-  INSERT INTO Cliente (cliNom, cliMail, cliCantCargas)
-VALUES ('John Doe', 'jleonardodavinci@mail', 5); -- EL MAIL YA EXISTE
-
-INSERT INTO Avion (avionID, avionMAT, avionMarca, avionModelo, avionCapacidad)
-VALUES ('AV001', 'ABC123', 'Boeing', '747', 200); --VIOLA LA CAPACIDAD MÁXIMA POSIBLE
-
-INSERT INTO Dcontainer (dContID, dContLargo, dContAncho, dcontAlto, dcontCapacidad)
-VALUES ('DC001', 3.0, 2.0, 2.0, 5.0); --EL LARGO ES MAYOR A 2.5 MTS
-
-INSERT INTO Aeropuerto (codIATA, aeroNombre, aeroPais)
-VALUES ('AER010', 'Aeropuerto de Viena-Schwechat', 'Austria'); --ERROR DE PK Aeropuerto
 
 INSERT INTO Carga (avionID, dContID, cargaFch, cargaKilos, cliID, aeroOrigen, aeroDestino, cargaStatus)
-VALUES ('AV001', 'DC001', '2023-05-01', 1000, 100, 'AER001', 'AER002', 'R'); -,--el Cliente no existe, viola la foreign key
+VALUES 
+  ('AVN001', 'DC001', '10/05/2023', 10000, 1, 'AER001', 'AER002', 'R'),
+  ('AVN001', 'DC002', '12/05/2023', 8000, 1, 'AER001', 'AER003', 'R'),
+  ('AVN002', 'DC003', '11/05/2023', 5000, 2, 'AER002', 'AER003', 'C'),
+  ('AVN003', 'DC004', '13/05/2023', 7000, 3, 'AER003', 'AER004', 'R'),
+  ('AVN004', 'DC005', '14/05/2023', 4000, 4, 'AER004', 'AER005', 'C'),
+  ('AVN005', 'DC006', '15/05/2023', 3000, 5, 'AER005', 'AER006', 'R'),
+  ('AVN006', 'DC007', '16/05/2023', 6000, 6, 'AER006', 'AER007', 'C'),
+  ('AVN007', 'DC008', '17/05/2023', 9000, 7, 'AER007', 'AER008', 'R'),
+  ('AVN008', 'DC009', '18/05/2023', 2000, 8, 'AER008', 'AER009', 'C'),
+  ('AVN009', 'DC010', '19/05/2023', 5000, 9, 'AER009', 'AER010', 'R');
+
+
+/*ESTOS DARIAN ERROR*/
+INSERT INTO Cliente (cliNom, cliMail, cliCantCargas)
+VALUES ('John Doe', 'jleonardodavinci@mail.com', 5); -- EL MAIL YA EXISTE
+
+INSERT INTO Avion (avionID, avionMAT, avionMarca, avionModelo, avionCapacidad)
+VALUES ('AV001', 'ABC123', 'Boeing', '747', 200); -- VIOLA LA CAPACIDAD MAXIMA POSIBLE
+
+INSERT INTO Dcontainer (dContID, dContLargo, dContAncho, dcontAlto, dcontCapacidad)
+VALUES ('DC001', 3.0, 2.0, 2.0, 5.0); -- EL LARGO ES MAYOR A 2.5 MTS
+
+INSERT INTO Aeropuerto (codIATA, aeroNombre, aeroPais)
+VALUES ('AER010', 'Aeropuerto de Viena-Schwechat', 'Austria'); -- ERROR DE PK Aeropuerto
+
+INSERT INTO Carga (avionID, dContID, cargaFch, cargaKilos, cliID, aeroOrigen, aeroDestino, cargaStatus)
+VALUES ('AV001', 'DC001', '01/05/2023', 1000, 100, 'AER001', 'AER002', 'R'); -- El Cliente no existe, viola la foreign key
 
 
 
